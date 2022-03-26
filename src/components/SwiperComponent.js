80544753e06379f9e975e02abfac016087d8f475
ksy7533 <ksy7533@gmail.com>
2022-03-26 15:23:39 +0900

    스와이퍼 네비 버튼 커스터마이징

--- src/components/SwiperComponent.js
+++ src/components/SwiperComponent.js
@@ -2,18 +2,16 @@ import React, { useState, useRef, useMemo, useCallback } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import styled, { css } from "styled-components";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 import RWD from "../styles/rwd";
-
+import Colors from "../styles/colors";
 import "swiper/css";
-import "swiper/css/navigation";
-
 import img01 from "../images/img_01.jpg";
 import img02 from "../images/img_02.jpg";
 import img03 from "../images/img_03.jpg";
 import img04 from "../images/img_04.jpg";
 import img05 from "../images/img_05.jpg";
 import img06 from "../images/img_06.jpg";
-import Colors from "../styles/colors";
 
 const photoList = [
   {
@@ -98,12 +96,59 @@ const Styled = {
     }
   `,
 
-  PhotoSwiperWrap: styled.div``
+  PhotoSwiperWrap: styled.div`
+    position: relative;
+
+    button {
+      z-index: 10;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+
+      &.prev-button {
+        left: 1rem;
+      }
+
+      &.next-button {
+        right: 1rem;
+      }
+
+      .icon {
+        color: ${Colors.WHITE};
+        font-size: 4rem;
+      }
+
+      &.swiper-button-disabled {
+        .icon {
+          opacity: 0.4;
+        }
+      }
+    }
+  `,
+
+  PrevArrowIcon: styled(HiChevronDoubleLeft)`
+    color: ${Colors.WHITE};
+  `,
+
+  NextArrowIcon: styled(HiChevronDoubleRight)`
+    color: ${Colors.WHITE};
+  `,
+
+  ScreenOut: styled.span`
+    overflow: hidden;
+    position: absolute;
+    clip: rect(0 0 0 0);
+    margin: -1px;
+    width: 1px;
+    height: 1px;
+  `
 };
 
 function SwiperComponent() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const photoSwiperRef = useRef();
+  const photoSwiperRef = useRef(null);
+  const prevButtonRef = useRef(null);
+  const nextButtonRef = useRef(null);
 
   const handleClickTag = useCallback(
     id => {
@@ -117,6 +162,13 @@ function SwiperComponent() {
     },
     [photoSwiperRef]
   );
+
+  const handlePhotoSwiperInit = useCallback(swiper => {
+    swiper.params.navigation.prevEl = prevButtonRef.current;
+    swiper.params.navigation.nextEl = nextButtonRef.current;
+    swiper.navigation.init();
+    swiper.navigation.update();
+  }, []);
 
   const renderTagSwiperSlide = useMemo(() => {
     return () => {
@@ -162,13 +214,26 @@ function SwiperComponent() {
       <Styled.PhotoSwiperWrap>
         <Swiper
           onSwiper={swiper => (photoSwiperRef.current = swiper)}
-          navigation={true}
           modules={[Navigation]}
           className="photo-swiper"
           onRealIndexChange={handleRealIndexChange}
+          navigation={{
+            prevEl: prevButtonRef.current,
+            nextEl: nextButtonRef.current
+          }}
+          onInit={handlePhotoSwiperInit}
         >
           {renderPhotoSwiperSlide()}
         </Swiper>
+
+        <button ref={prevButtonRef} className="prev-button">
+          <HiChevronDoubleLeft className="icon" />
+          <Styled.ScreenOut>이전</Styled.ScreenOut>
+        </button>
+        <button ref={nextButtonRef} className="next-button">
+          <HiChevronDoubleRight className="icon" />
+          <Styled.ScreenOut>다음</Styled.ScreenOut>
+        </button>
       </Styled.PhotoSwiperWrap>
     </>
   );
